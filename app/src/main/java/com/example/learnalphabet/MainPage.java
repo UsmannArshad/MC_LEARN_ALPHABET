4baf39df04a08e5c75fc1126481f807bc58605c2
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-05-21 14:29:13 +0500

    Implementation of repository button

--- app/src/main/java/com/example/learnalphabet/MainPage.java
+++ app/src/main/java/com/example/learnalphabet/MainPage.java
@@ -3,19 +3,21 @@ package com.example.learnalphabet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.view.View;
 import android.widget.Button;
 
 import android.os.Bundle;
 
 public class MainPage extends AppCompatActivity {
-    private Button learnbutton,testbutton;
+    private Button learnbutton,testbutton,repobutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
         learnbutton=findViewById(R.id.learn);
         testbutton=findViewById(R.id.test);
+        repobutton=findViewById(R.id.repo);
         learnbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,6 +31,15 @@ public class MainPage extends AppCompatActivity {
                 Intent intent=new Intent(MainPage.this,TestActivity.class);
                 intent.putExtra("score",0.0);
                 intent.putExtra("wrong",0.0);
+                startActivity(intent);
+            }
+        });
+        repobutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url="https://github.com/UsmannArshad/MC_LEARN_ALPHABET";
+                Uri webpage=Uri.parse(url);
+                Intent intent=new Intent(Intent.ACTION_VIEW,webpage);
                 startActivity(intent);
             }
         });
