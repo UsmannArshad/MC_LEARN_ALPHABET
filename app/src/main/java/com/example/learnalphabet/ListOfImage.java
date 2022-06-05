68c4830a41689a62a97fe41c637421aab868139b
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-06-05 09:01:24 +0500

    files added for customized layout

---
+++ app/src/main/java/com/example/learnalphabet/ListOfImage.java
@@ -0,0 +1,14 @@
+package com.example.learnalphabet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListOfImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_image);
+    }
+}
